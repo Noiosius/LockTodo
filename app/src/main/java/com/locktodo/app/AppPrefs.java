86201c13eb727c7beb76663b062c16580975d3a1
Noiosius <leehoyoug0326@gmail.com>
2026-09-12 12:41:38 +0900

    Refocus preferences on resizable widget appearance

--- app/src/main/java/com/locktodo/app/AppPrefs.java
+++ app/src/main/java/com/locktodo/app/AppPrefs.java
@@ -6,38 +6,26 @@ import android.content.SharedPreferences;
 final class AppPrefs {
     static final String PREFS = "lock_todo_prefs";
 
-    static final String KEY_AUTO_SHOW = "auto_show";
     static final String KEY_SHOW_PLUS = "show_plus";
     static final String KEY_HIDE_PLUS_WHEN_EMPTY = "hide_plus_when_empty";
+    static final String KEY_SHOW_EMPTY_PANEL = "show_empty_panel";
     static final String KEY_HAPTIC = "haptic";
     static final String KEY_LIGHT_TEXT = "light_text";
 
-    static final String KEY_PANEL_WIDTH = "panel_width_pct";
-    static final String KEY_PANEL_HEIGHT = "panel_height_dp";
-    static final String KEY_PANEL_X = "panel_x_pct";
-    static final String KEY_TOP_OFFSET = "top_offset_dp";
-
-    static final String KEY_ROW_HEIGHT = "row_height_dp";
-    static final String KEY_ROW_GAP = "row_gap_dp";
-    static final String KEY_ROW_PADDING = "row_padding_dp";
-
     static final String KEY_TEXT_SIZE = "text_size_sp";
     static final String KEY_TEXT_ALPHA = "text_alpha";
-    static final String KEY_BORDER_ALPHA = "border_alpha";
-    static final String KEY_BORDER_WIDTH = "border_width_dp";
     static final String KEY_FILL_ALPHA = "fill_alpha";
-    static final String KEY_CORNER_RADIUS = "corner_radius_dp";
+    static final String KEY_BORDER_ALPHA = "border_alpha";
+    static final String KEY_ROW_PADDING = "row_padding_dp";
+    static final String KEY_HORIZONTAL_PADDING = "horizontal_padding_dp";
+    static final String KEY_MAX_ROWS = "max_rows";
 
-    static final String KEY_CHECK_SIZE = "check_size_sp";
     static final String KEY_CHECK_ALPHA = "check_alpha";
-    static final String KEY_HANDLE_SIZE = "handle_size_sp";
     static final String KEY_HANDLE_ALPHA = "handle_alpha";
     static final String KEY_PLUS_SIZE = "plus_size_sp";
     static final String KEY_PLUS_ALPHA = "plus_alpha";
-    static final String KEY_PLUS_END_MARGIN = "plus_end_margin_dp";
-    static final String KEY_PLUS_BOTTOM_MARGIN = "plus_bottom_margin_dp";
-
     static final String KEY_CHECK_DELAY = "check_delay_ms";
+    static final String KEY_CHECKING_INDEX = "checking_index";
 
     private AppPrefs() {}
 
